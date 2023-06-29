9064e2b8b649459adb16e68aab0b4568bcf982ae
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-06-29 08:22:01 +0100

    fix: username not defined

--- api/index.js
+++ api/index.js
@@ -102,7 +102,7 @@ app.get('/post', async (req, res) => {
 
 app.get('/post/:id', async (req, res) => {
     const {id} = req.params;
-    const postDoc = await Post.findById(id);
+    const postDoc = await Post.findById(id).populate('author', ['username']);
     res.json(postDoc);
 })
 
--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -19,7 +19,7 @@ export default function PostPage(){
         <div className="post-page">
             <h1>{postInfo.title}</h1>
             <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
-            <div className='author'>by @{postInfo.author.username}</div>
+            <div className='author'>by @{postInfo.author?.username}</div>
             <div className='image'>
                 <img src={`http://localhost:4000/${postInfo.cover}`} alt='' />
             </div>
